227e88f804acc2751d123f3134ddc08873ab7a6e
xuwanwan <xf@jusfoun.com>
2018-03-26 17:52:27 +0800

    feat:添加tokens

--- src/pages/introduce/introduce.js
+++ src/pages/introduce/introduce.js
@@ -4,15 +4,27 @@ import * as api from "../../api/test"
 class Introduce extends Component {
   constructor() {
     super();
+    this._tokens = [];
   }
 
   componentDidMount() {
-    api.test.send({
+    let me = this;
+    me._tokens.push(api.test.send({
       name: "xxx",
       test: "ooo"
     }).then(res => {
       console.log(res);
-    })
+    }));
+  }
+
+  _clearTokens() {
+    console.log(this._tokens[0])
+    // this._tokens.forEach(token => token.cancel());
+    this._tokens = [];
+  };
+
+  componentWillUnmount() {
+    this._clearTokens();
   }
 
   render() {
